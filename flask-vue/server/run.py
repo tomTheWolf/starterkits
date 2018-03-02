30e746c1b7f0e9d6b228b732c87fb32af2c53123
Tom Gellatly <tom@side.cr>
2018-03-02 16:19:36 -0500

    better dir names

--- flask-vue/server/run.py
+++ flask-vue/server/run.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder = "../frontend/dist/static", template_folder = "../frontend/dist")
+app = Flask(__name__, static_folder = "../client/dist/static", template_folder = "../client/dist")
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
